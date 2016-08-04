755001cc7fac568fcffa7b1b702a8395b93080b4
Liu Qing <liuqing@chinac.com>
2016-08-04 10:17:37 +0000

    remove unused plugin
    Signed-off-by: Liu Qing <liuqing@chinac.com>

---
+++ add_submodels.sh
@@ -0,0 +1,6 @@
+git submodule add https://github.com/vim-scripts/pep8.git bundle/pep8
+git submodule add https://github.com/tpope/vim-git.git bundle/git
+git submodule add https://github.com/vim-scripts/The-NERD-tree.git bundle/nerdtree
+git submodule add http://github.com/tpope/vim-fugitive.git bundle/fugitive
+git submodule add https://github.com/hynek/vim-python-pep8-indent.git bundle/vim-python-pep8-indent
+git submodule add https://github.com/altercation/solarized.git colors/solarized
