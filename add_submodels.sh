ea70374bfa20e2adb314e384b65f52dd2613e253
Liu Qing <liuqing@chinac.com>
2016-08-05 01:36:03 +0000

    add jedi-vim submodule
    Signed-off-by: Liu Qing <liuqing@chinac.com>

--- add_submodels.sh
+++ add_submodels.sh
@@ -4,3 +4,4 @@ git submodule add https://github.com/vim-scripts/The-NERD-tree.git bundle/nerdtr
 git submodule add http://github.com/tpope/vim-fugitive.git bundle/fugitive
 git submodule add https://github.com/hynek/vim-python-pep8-indent.git bundle/vim-python-pep8-indent
 git submodule add https://github.com/altercation/solarized.git colors/solarized
+git submodule add https://github.com/davidhalter/jedi-vim.git bundle/jedi-vim
