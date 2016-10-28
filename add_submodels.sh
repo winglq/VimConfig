726772a1a066f7252f96d8e8ad1ca9b538913bcc
Liu Qing <liuqing@chinac.com>
2016-10-28 17:45:08 +0800

    add vim-go
    Signed-off-by: Liu Qing <liuqing@chinac.com>

--- add_submodels.sh
+++ add_submodels.sh
@@ -5,3 +5,4 @@ git submodule add http://github.com/tpope/vim-fugitive.git bundle/fugitive
 git submodule add https://github.com/hynek/vim-python-pep8-indent.git bundle/vim-python-pep8-indent
 git submodule add https://github.com/altercation/solarized.git colors/solarized
 git submodule add https://github.com/davidhalter/jedi-vim.git bundle/jedi-vim
+git submodule add https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
